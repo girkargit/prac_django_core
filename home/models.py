553b2abb92bd73299b27e79975045abde13caa46
girkargit <abhilashgirkar97@gmail.com>
2024-05-05 21:25:37 +0530

    Changes

--- home/models.py
+++ home/models.py
@@ -8,7 +8,12 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     email = models.EmailField(null=True, blank=True)
-    address = models.TextField(null=True, blank=True)   
+    address = models.TextField(null=True, blank=True)
 
-class Produnt(models.Model):
-    pass
+
+class Car(models.Model):
+    car_name = models.CharField(max_length=500)
+    speed = models.IntegerField(default=50)
+
+    def __str__(self) -> str:
+        return self.car_name
